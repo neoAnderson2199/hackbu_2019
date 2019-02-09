81e9df1f892d382859feea36b34046a115a3b4f0
Nicholas Bunnell <nbunnel1@binghamton.edu>
2019-02-09 14:13:19 -0500

    first commit

---
+++ Answer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class Answer {
+	private Array_List<String> keywords;
+	
+	public void take_in(String input_file) {
+		 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hackbu_2019
+
+
+List of possible responses from user given the 5 sense:
+Sight: blue, green

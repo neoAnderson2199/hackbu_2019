f61c114ed6c8e46b69d5b18ff62fcf32873c7d7c
Teresa Chu <tchu4@binghamton.edu>
2019-02-09 14:49:31 -0500

    "Added field to add text"

--- Menu.java
+++ Menu.java
@@ -12,6 +12,26 @@ public class Menu {
 				       frame.setSize(800,600);
 				       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
 				       frame.setVisible(true);
+				       
+						JPanel panel = new JPanel();
+						
+						//Area to Text 
+						JTextArea textArea = new JTextArea();
+						JTextField textField = new JTextField(20);
+					
+						//Labels
+						JLabel label = new JLabel("Enter Text: ");
+						
+						//Buttons
+						JButton enter = new JButton("Enter");
+
+						//Add To Panel and Frame
+						panel.add(label);
+						panel.add(textField);
+						panel.add(enter);
+						
+						frame.getContentPane().add(BorderLayout.SOUTH,panel);
+						frame.getContentPane().add(BorderLayout.CENTER,textArea);
 				}
 			});
 		

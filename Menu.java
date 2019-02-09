1f7d4f638f5dd7af80bd8f65294a8150b5e275b7
BettyZhan <xzhan215@bignhamton.edu>
2019-02-09 15:20:22 -0500

    add more

--- Menu.java
+++ Menu.java
@@ -1,41 +1,47 @@
 package hackbu_2019;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
 public class Menu {
 	public static void main(String args[]){
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					//Set the frame
-				       JFrame frame = new JFrame("Advice Giver"); //Setting name
-				       frame.setSize(800,600);
-				       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
-				       frame.setVisible(true);
-				       
-						JPanel panel = new JPanel();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+			//Set the frame
+				JFrame frame = new JFrame("Advice Giver"); //Setting name
+				frame.setSize(800,600);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
+				frame.setVisible(true);
+				
+				JPanel panel = new JPanel();
 						
-						//Area to Text 
-						JTextArea textArea = new JTextArea();
-						JTextField textField = new JTextField(20);
+			//Area to Text 
+				JTextArea textArea = new JTextArea();
+				JTextField textField = new JTextField(20);
 					
-						//Labels
-						JLabel label = new JLabel("Enter Text: ");
+			//Labels
+				JLabel label = new JLabel("Enter Text: ");
 						
-						//Buttons
-						JButton enter = new JButton("Enter");
-
-						//Add To Panel and Frame
-						panel.add(label);
-						panel.add(textField);
-						panel.add(enter);
+			//Buttons
+				JButton enter = new JButton("Enter");
+				
+			//Button click and return text
+				enter.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent ae){
+						String getValue = textField.getText();
+						System.out.println(getValue);
+					}
+				});
+				
+			//Add To Panel and Frame
+				panel.add(label);
+				panel.add(textField);
+				panel.add(enter);
 						
-						frame.getContentPane().add(BorderLayout.SOUTH,panel);
-						frame.getContentPane().add(BorderLayout.CENTER,textArea);
-				}
-			});
-		
-	       
+				frame.getContentPane().add(BorderLayout.SOUTH,panel);
+				frame.getContentPane().add(BorderLayout.CENTER,textArea);
+			}
+		});
 	}
-	
 }

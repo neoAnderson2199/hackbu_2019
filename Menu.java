4bad34851a97d503d0d46a66761310971e549c17
Teresa Chu <tchu4@binghamton.edu>
2019-02-09 14:28:35 -0500

    "added to menu"

--- Menu.java
+++ Menu.java
@@ -1,12 +1,21 @@
 package hackbu_2019;
 import javax.swing.*;
+import java.awt.*;
+
 
 public class Menu {
 	public static void main(String args[]){
-	       JFrame frame = new JFrame("HR Responder");
-	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(800,800);
-	       frame.setVisible(true);
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					//Set the frame
+				       JFrame frame = new JFrame("Advice Giver"); //Setting name
+				       frame.setSize(800,600);
+				       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
+				       frame.setVisible(true);
+				}
+			});
+		
+	       
 	}
 	
 }

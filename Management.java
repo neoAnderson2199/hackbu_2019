d4df4620eeb827c814bf0df139977f1ed9b12c64
BettyZhan <xzhan215@bignhamton.edu>
2019-02-09 16:35:40 -0500

    more add

---
+++ Management.java
@@ -0,0 +1,40 @@
+package hackbu_2019;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Management{
+	final JFrame frame = new JFrame("Advice Giver");
+	private JPanel panel = new JPanel();
+	
+	public void initialize() {
+		frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
+		frame.setVisible(true);
+		buildPanel();
+		frame.getContentPane().add(panel);
+	}
+	
+	public void buildPanel() {
+						
+		//Area to Text 
+		JTextField textField = new JTextField(20);
+					
+		//Labels
+		JLabel label = new JLabel("Enter Text: ");
+						  
+		//Buttons
+		JButton enter = new JButton("Enter");
+			
+		//Add To Panel and Frame
+		panel.add(label);
+		panel.add(textField);
+		panel.add(enter);
+	}
+	/*
+	public String getFieldValue(String fieldName){
+        JTextField field = panel.get(fieldName);
+        return field.getText();
+    }
+	 */
+}
--- Menu.java
+++ Menu.java
@@ -1,47 +1,34 @@
 package hackbu_2019;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 
 public class Menu {
-	public static void main(String args[]){
-		SwingUtilities.invokeLater(new Runnable() {
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(
+			UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+
+		//Schedule a job for the event-dispatching thread:
+ 	    //creating and showing this application's GUI.
+ 		// see http://download.oracle.com/javase/tutorial/uiswing/concurrency/initial.html
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-			//Set the frame
-				JFrame frame = new JFrame("Advice Giver"); //Setting name
-				frame.setSize(800,600);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			       
-				frame.setVisible(true);
-				
-				JPanel panel = new JPanel();
-						
-			//Area to Text 
-				JTextArea textArea = new JTextArea();
-				JTextField textField = new JTextField(20);
-					
-			//Labels
-				JLabel label = new JLabel("Enter Text: ");
-						  
-			//Buttons
-				JButton enter = new JButton("Enter");
-				
-			//Button click and return text
-				enter.addActionListener(new ActionListener(){
-					public void actionPerformed(ActionEvent ae){
-						String getValue = textField.getText();
-						System.out.println(getValue);
-					}
-				});
-				
-			//Add To Panel and Frame
-				panel.add(label);
-				panel.add(textField);
-				panel.add(enter);
-						
-				frame.getContentPane().add(BorderLayout.SOUTH,panel);
-				frame.getContentPane().add(BorderLayout.CENTER,textArea);
-			}
-		});
-	}
+				int i = JOptionPane.showConfirmDialog(null, "THIS APPLICATION FILLS YOUR SCREEN\n" +
+ 	        			"Do not run it until you know how to close\n" +
+ 	        			"an application from the keyboard.\n" +
+ 	        			"On Linux and Windows use ALT-F4.\n" +
+ 	        			"On Mac, it may be Command+W\n" +
+ 	        			"--test it using another window first!");
+ 	        	if(i == JOptionPane.OK_OPTION) {
+ 	        		Management screen = new Management();
+ 	        		screen.initialize();
+ 	        	}
+ 	        }
+ 	    });
+ 	}
 }
+
